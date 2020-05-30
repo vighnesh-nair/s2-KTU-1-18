50f439f3958ff2cb7ea99db5ef936b63fffb1c5d
vighnesh-nair <62141079+vighnesh-nair@users.noreply.github.com>
2020-05-30 15:08:45 +0530

    Create 11_palindrome.c

---
+++ 11_palindrome.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+	{
+	        	char str[17];
+         		int flag = 0;
+
+		        printf("\n Enter the string (word): ");
+		         scanf("%s", str);
+
+		        int i = 0;
+		        int j = strlen(str)-1;
+
+	        	while(j>i)
+		           	{
+			          	if(str[i++] != str[j--])
+					          {
+					            	flag = 1;
+					            	break;
+					          }
+			           }
+		
+	        	if(flag == 1 )
+			          printf("\n Not a Palindrome...\n");
+	        	else
+		          	printf("\n PALINDROME! \n");
+	  }
