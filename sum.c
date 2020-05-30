eb80d275f316c470c94b30b335ab326b28c493f0
vighnesh-nair <62141079+vighnesh-nair@users.noreply.github.com>
2020-05-30 12:26:53 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,15 @@
+  
+/* to read two numbers, add them and display their sum */
+
+#include<stdio.h>
+
+void main()
+	{ 
+	   	int num1,num2,sum;
+		  printf("\n Enter the two numbers:\n");
+		  scanf("%d%d", &num1, &num2);
+	  
+	  	sum = num1+num2;
+	  
+	  	printf("Sum = %d  \n", sum);
+  }
